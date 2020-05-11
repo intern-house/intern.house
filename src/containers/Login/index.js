74158923e9fef693d7bf444dfa7226a890da12a0
Swapnil Patel <swapnilpatel3526@gmail.com>
2020-05-10 22:33:16 -0500

    Removed SignUpContainer and SignInContainer from Login/index.js

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -3,14 +3,6 @@ import { Container } from './styles';
 import SignInContainer from './Sign-In';
 import SignUpContainer from './Sign-Up';
 
-const SignUpContainer = () => {
-	return <Container>Sign Up</Container>;
-};
-
-const SignInContainer = () => {
-	return <Container>Sign In</Container>;
-};
-
 const ForgetPasswordContainer = () => {
 	return <Container>Forget Password</Container>;
 };
