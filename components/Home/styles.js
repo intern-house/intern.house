0feb2c32b42acfb7319a286921bbe9d9ce438ad9
Harsh-Topi <topiwalah@hotmail.com>
2020-07-26 22:20:55 -0400

    formatted modified files

--- components/Home/styles.js
+++ components/Home/styles.js
@@ -1,19 +1,17 @@
 import styled from 'styled-components';
 
-
 export const Background = styled.div`
-	background-color: #F6FBFF;
+	background-color: #f6fbff;
 	height: 100vh;
 `;
 
 // To show a white box behind container
 // 	background-color: white;
-//	box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.25); 
+//	box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.25);
 export const Container = styled.div`
 	position: relative;
 	width: 1200px;
 	margin: 7.5vh auto 2vh;
-	
 
 	@media (max-width: 1200px) {
 		margin: 3.5vh auto 2vh;
@@ -25,7 +23,7 @@ export const HeroImg = styled.img`
 	display: block;
 	margin: auto;
 	padding-left: 25%;
-	
+
 	@media (max-width: 1000px) {
 		width: 100%;
 	}
